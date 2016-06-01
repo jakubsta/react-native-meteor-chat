a00036e0e656fcad55ac97713b8a57f8fd91925d
Jakub Stasiak <kubafr2@gmail.com>
2016-06-01 16:25:11 +0200

    moved to one main controller

--- chat/ios/chat/AppDelegate.m
+++ chat/ios/chat/AppDelegate.m
@@ -31,7 +31,11 @@
    * on the same Wi-Fi network.
    */
 
+<<<<<<< Updated upstream
   jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle?platform=ios&dev=true"];
+=======
+  jsCodeLocation = [NSURL URLWithString:@"http://192.168.43.215:8081/index.ios.bundle?platform=ios&dev=true"];
+>>>>>>> Stashed changes
 
   /**
    * OPTION 2
