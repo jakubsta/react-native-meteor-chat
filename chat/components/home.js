271112e46ea533c90a53d1a8480c00d3a33a8a93
Jakub Stasiak <kubafr2@gmail.com>
2016-06-01 17:37:15 +0200

    basic navigation

---
+++ chat/components/home.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import Button from 'react-native-button';
+
+
+export class Home extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Home</Text>
+        <Button
+          onPress={() => this.props.navigator.push({name: 'login'})}>
+          Go to login
+        </Button>
+        <Button>
+          Go to signup
+        </Button>
+        <Button>
+          Go to rooms 
+        </Button>
+      </View>
+  );
+  }
+}
+
+const styles = StyleSheet.create({
+  input : {
+    textAlign: 'center',
+    width: 100,
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1
+  }
+});
--- chat/components/main.js
+++ chat/components/main.js
@@ -2,10 +2,12 @@ import React, { Component } from 'react';
 import {
   StyleSheet,
   Text,
-  View
+  View,
+  Navigator
 } from 'react-native';
 import Meteor, { createContainer, Accounts } from 'react-native-meteor';
 import { Login } from './login';
+import { Home } from './home';
 
 
 Meteor.connect('http://192.168.43.58:3000/websocket');
@@ -13,19 +15,21 @@ Meteor.connect('http://192.168.43.58:3000/websocket');
 export class Chat extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
-        </Text>
-        <Login />
-      </View>
+      <Navigator 
+        initialRoute={{name: 'home'}}
+        renderScene={this.renderScene}/>
     );
+  }
+
+  renderScene(route, navigator) {
+    switch(route.name) {
+      case 'home': 
+        return <Home navigator={navigator}/>;  
+      case 'login':
+        return <Login/>;
+      case 'signup':
+        return <View></View>;
+    }
   }
 }
 
